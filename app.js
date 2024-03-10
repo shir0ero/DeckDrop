471f2cf84efecfbda3070c6b70f4295011bb7d23
shir0ero <maiayushrajhu@gmail.com>
2024-03-10 15:55:20 +0530

    added title

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { pageTitle: 'Page not found' });
 })
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     // res.send("<h1>I love pokemons</h1>")
     const products = adminData.products;
-    res.render('shop', { prods: products, doctitle: 'shop' });
+    res.render('shop', { prods: products, pageTitle: 'shop', path: '/' });
 });
 //well __dirname will point to the routes folder(where are we using it)
 //but views is a sibling folder
