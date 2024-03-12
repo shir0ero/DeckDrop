ee952988fbe95d62c3459c389042458f167c8bf7
shir0ero <maiayushrajhu@gmail.com>
2024-03-12 19:34:24 +0530

    Added cart and product headers

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         formsCSS: true,
@@ -19,7 +19,7 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
     // res.send("<h1>I love pokemons</h1>"
     const products = Product.fetchAll(products => {
-        res.render('shop', {
+        res.render('shop/product-list', {
             prods: products,
             pageTitle: 'shop',
             path: '/',
