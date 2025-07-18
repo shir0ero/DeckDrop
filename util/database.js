b5ffe7fd4cc6eb7b26a20e7854266669e31f436c
shir0ero <maiayushrajhu@gmail.com>
2025-07-19 04:31:11 +0530

    Update Sequelize config to use DATABASE_URL for Render deployment

--- util/database.js
+++ util/database.js
@@ -1,16 +1,13 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-        dialect: 'postgres', // Changed from mysql to postgres
-        host: process.env.DB_HOST,
-        dialectOptions: {
-            ssl: process.env.NODE_ENV === 'production' ? { require: true, rejectUnauthorized: false } : false
-        }
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production'
+      ? { require: true, rejectUnauthorized: false }
+      : false
+  }
+});
 
 module.exports = sequelize;
